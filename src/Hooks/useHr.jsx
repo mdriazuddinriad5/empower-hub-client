f6fa33dc411ba7c5872d1a65a8fb7ba22acf1574
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-11-29 01:36:43 +0600

    useAdmin and useHr hook added

---
+++ src/Hooks/useHr.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+
+
+const useHr = () => {
+    const axiosSecure = useAxiosSecure();
+    const { user, loading } = useAuth();
+    const { data: isHr, isPending: isHrLoading } = useQuery({
+        queryKey: [user?.email, 'isHr'],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/hr/${user.email}`);
+            console.log(res.data);
+            return res.data?.hr;
+        }
+    })
+
+    return [isHr, isHrLoading]
+};
+
+export default useHr;
